d82be3819737c9a424b6a2c65565f85c34d853b6
kaylee-ke <440006455@gapps.yrdsb.ca>
2023-05-29 16:27:31 -0400

    add the rocket

--- MyWorld.java
+++ MyWorld.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * the sky
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @Kaylee Ke
+ * @May 29
  */
 public class MyWorld extends World
 {
@@ -17,5 +17,8 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        
+        Rocket rocket = new Rocket();
+        addObject(rocket, 55, 200);
     }
 }
