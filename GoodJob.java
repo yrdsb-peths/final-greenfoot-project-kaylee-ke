883213ed596d7d0beb6d6cd8d418e1765b994299
kaylee-ke <440006455@gapps.yrdsb.ca>
2023-06-02 09:00:20 -0400

    improvement to the winning screen

---
+++ GoodJob.java
@@ -0,0 +1,38 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * continue or end the game
+ * 
+ * @Kaylee Ke 
+ * @June 2
+ */
+public class GoodJob extends Actor
+{
+    GreenfootImage image = new GreenfootImage("images/goodJob.png");
+    
+    public void act()
+    {
+        image.scale(450,150);
+        setImage(image);
+        
+        //press 9 to continue, press 0 to end the game
+        checkKeyPress();
+    }
+    
+    private void checkKeyPress()
+    {
+        if(Greenfoot.isKeyDown("9"))
+        {
+            switchToWorld(new MyWorld());
+        }
+        else if(Greenfoot.isKeyDown("0"))
+        {
+            Greenfoot.stop();
+        }
+    }
+    
+    private void switchToWorld(World newWorld)
+    {
+        Greenfoot.setWorld(newWorld);
+    }
+}
--- WinningScreen.java
+++ WinningScreen.java
@@ -8,16 +8,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class WinningScreen extends World
 {
-    Label winningLabel = new Label("Good Job", 100);
-    /**
-     * Constructor for objects of class WinningScreen.
-     * 
-     */
+    Label labelOne = new Label("press 9 to continue;", 50);
+    Label labelTwo = new Label("press 0 to end the game.", 50);
+    
     public WinningScreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
-        
-        addObject(winningLabel, getWidth()/2, 150);
+        super(600, 400, 1);
+        createLabel();
+        addObject(labelOne, 300, 270);
+        addObject(labelTwo, 300, 330);
+    }
+    
+    public void createLabel()
+    {
+        GoodJob good = new GoodJob();
+        int x = 300;
+        int y = 120;
+        addObject(good, x, y);
     }
 }
